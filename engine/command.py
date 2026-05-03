4251d4444f2b58e7725a0f71e4e045b23d75f3e7
Ayush Mishra <aayumishra2024@gmail.com>
2026-05-03 21:26:07 +0530

    day 7 task completed successfully

--- engine/command.py
+++ engine/command.py
@@ -1,5 +1,7 @@
 import pyttsx3
+import speech_recognition as sr
 
+# function for the ai to speak
 def speak(text):
     engine = pyttsx3.init()
     voices = engine.getProperty('voices')
@@ -8,5 +10,28 @@ def speak(text):
     engine.setProperty('rate', 174)
     engine.say(text)
     engine.runAndWait()
+
+
+# function for the ai to listen the command of the user and speak that command
+def takecommand():
     
-speak("I love India")
+    r = sr.Recognizer()
+    
+    with sr.Microphone() as source:
+        print("listening.....")
+        r.pause_threshold = 1
+        r.adjust_for_ambient_noise(source)
+        
+        audio = r.listen( source , 10 , 6 )
+    
+    try:
+        print('recognizing')
+        query = r.recognize_google( audio , language='en-in')
+        print(f"user said : {query}")
+    except Exception as e :
+        return ""
+    return query.lower()
+
+
+text = takecommand()
+speak(text)
